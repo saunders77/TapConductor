6bf2110306ed2499852667701f33c83afac0bd8f
Michael Saunders <michaelsaunders@outlook.com>
2026-08-08 16:06:46 -0700

    fixed title bar colors

--- src/main.ts
+++ src/main.ts
@@ -22,6 +22,7 @@ import {
   isWebBuild,
   openScoreDialog,
   setAppWindowTitle,
+  syncAppWindowThemeWithSystem,
   type UnlistenFn,
 } from "./platform";
 import { detectAppleUiPlatform, initialTelemetryAction } from "./ui-platform";
@@ -523,6 +524,9 @@ let scorePerformance: ScorePerformance | null = null;
 let lastDiagnostics: DiagnosticsDto | null = null;
 let lastUiNativeRoundTripMs: number | null = null;
 let unlisteners: UnlistenFn[] = [];
+if (appleUiPlatform === "macos") {
+  unlisteners.push(syncAppWindowThemeWithSystem());
+}
 const heldTokens = new Set<string>();
 let midiFreePlay = false;
 const DEFAULT_PIANO_SHORTCUT_PITCH = 36;
